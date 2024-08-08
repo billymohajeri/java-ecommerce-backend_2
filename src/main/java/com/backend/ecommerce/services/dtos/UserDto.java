42adbf4ace688530c702b5b212b43541a1b3c3fb
faizan <faizan.ah862@gmail.com>
2024-08-08 13:28:49 +0300

    Update user entity to have column definitions

--- src/main/java/com/backend/ecommerce/services/dtos/UserDto.java
+++ src/main/java/com/backend/ecommerce/services/dtos/UserDto.java
@@ -1,6 +1,8 @@
 package com.backend.ecommerce.services.dtos;
 
+import com.backend.ecommerce.entities.enums.AuthenticationRole;
+
 public record UserDto(String id, String firstName, String lastName, String address, String email, String password,
-                      String phoneNumber, String birthDate, String role) {
+                      String phoneNumber, String birthDate, AuthenticationRole role) {
 
 }
