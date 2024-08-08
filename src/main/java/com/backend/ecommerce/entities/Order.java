35c503e905db05c662dc0f7697a5edf531ddc18c
AA7776 <aa7776@student.jamk.fi>
2024-08-08 13:51:30 +0300

    Added order entity

---
+++ src/main/java/com/backend/ecommerce/entities/Order.java
@@ -0,0 +1,40 @@
+package com.backend.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order", schema = "ecomapp")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "date_time", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dateTime;
+
+    @Column(name = "comments")
+    private String comments;
+
+    @Column(name = "status", nullable = false)
+    private Integer status;
+
+    @Column(name = "address")
+    private String address;
+}
