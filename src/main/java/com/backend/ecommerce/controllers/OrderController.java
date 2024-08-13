21b42a8e428bcc91905b031a1ff16385124fd8d4
AA7776 <aa7776@student.jamk.fi>
2024-08-13 09:21:42 +0300

    Added error handling to Order

--- src/main/java/com/backend/ecommerce/controllers/OrderController.java
+++ src/main/java/com/backend/ecommerce/controllers/OrderController.java
@@ -2,7 +2,9 @@ package com.backend.ecommerce.controllers;
 
 import com.backend.ecommerce.dtos.order.OrderDto;
 import com.backend.ecommerce.dtos.order.OrderUpdateDto;
-import com.backend.ecommerce.services.OrderServiceImpl;
+import com.backend.ecommerce.services.interfaces.OrderService;
+import com.backend.ecommerce.shared.exceptions.CustomException;
+import com.backend.ecommerce.shared.response.GlobalResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,39 +19,44 @@ import java.util.UUID;
 public class OrderController {
 
     @Autowired
-    OrderServiceImpl orderService;
+    OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto){
+    public ResponseEntity<GlobalResponse<OrderDto>> createOrder(@RequestBody OrderDto orderDto){
         OrderDto createdOrder = orderService.createOrder(orderDto);
-        return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+        GlobalResponse<OrderDto> response = new GlobalResponse<>(createdOrder,null);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderDto> getOrderById(@PathVariable UUID id){
+    public ResponseEntity<GlobalResponse<OrderDto>> getOrderById(@PathVariable UUID id){
         Optional<OrderDto> order = orderService.getOrderById(id);
-        return order.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return order.map(dto -> ResponseEntity.ok(new GlobalResponse<>(dto, null)))
+                .orElseThrow(() -> new CustomException("Order not found! ", HttpStatus.NOT_FOUND.value()));
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderDto>> getAllOrders(){
+    public ResponseEntity<GlobalResponse<List<OrderDto>>> getAllOrders(){
         List<OrderDto> orders = orderService.getAllOrders();
-        return ResponseEntity.ok(orders);
+        GlobalResponse<List<OrderDto>> response = new GlobalResponse<>(orders, null);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OrderDto> updateOrder(@PathVariable UUID id, @RequestBody OrderUpdateDto orderDto){
+    public ResponseEntity<GlobalResponse<OrderDto>> updateOrder(@PathVariable UUID id, @RequestBody OrderUpdateDto orderDto){
         try{
             OrderDto updatedOrder = orderService.updateOrder(id, orderDto);
-            return ResponseEntity.ok(updatedOrder);
+            GlobalResponse<OrderDto> response = new GlobalResponse<>(updatedOrder, null);
+            return ResponseEntity.ok(response);
         }catch (RuntimeException e){
-            return ResponseEntity.notFound().build();
+            throw new CustomException("Order not found", HttpStatus.NOT_FOUND.value());
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<OrderDto> deleteOrder(@PathVariable UUID id){
+    public ResponseEntity<GlobalResponse<OrderDto>> deleteOrder(@PathVariable UUID id){
         orderService.deleteOrder(id);
+        GlobalResponse<Void> response = new GlobalResponse<>(null, null);
         return ResponseEntity.noContent().build();
     }
 }
