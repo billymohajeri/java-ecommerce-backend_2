a21902f285ad9ac394d190eb5c8b642389735472
AA7776 <aa7776@student.jamk.fi>
2024-08-14 16:51:50 +0300

    removed extra

--- src/main/java/com/backend/ecommerce/dtos/payment/PaymentUpdateDto.java
+++ src/main/java/com/backend/ecommerce/dtos/payment/PaymentUpdateDto.java
@@ -3,9 +3,6 @@ package com.backend.ecommerce.dtos.payment;
 import com.backend.ecommerce.entities.enums.PaymentMethod;
 import com.backend.ecommerce.entities.enums.PaymentStatus;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.UUID;
 
 public record PaymentUpdateDto(@DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than zero")Double amount,
                                PaymentStatus status,
